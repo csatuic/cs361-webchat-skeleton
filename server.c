393fdf3df61717253b49b6d1e41071f5041540e1
Chris Kanich <ckanich@uic.edu>
2021-03-28 17:50:57 -0500

    skeleton tweaks

--- server.c
+++ server.c
@@ -1,7 +1,7 @@
 
 #include "csapp.h"
 
-const char OK[] = "HTTP/1.1 200 OK\r\n\r\n";
+const char OK[] = "HTTP/1.1 200 OK\r\nContent-type: text/html; charset=UTF-8\r\n\r\n";
 
 const char NOTOK[] = "HTTP/1.1 400 Bad Request\r\n\r\n";
 
@@ -12,10 +12,10 @@ const char stream[] =
     "Connection: keep-alive\r\n\r\n"
     "retry: 10000\n\n";
 
-char indexhtml[8192];
+char indexhtml[4096];
 
 typedef struct {
-  char requestbuf[8192];
+  char requestbuf[4096];
   int first_empty_byte;
 } recv_buffer;
 
@@ -37,12 +37,12 @@ void check_clients(pool *p);
 
 void extract_message(char *src, char *dst) {
   // given an HTTP POST message containing a message, format it and
-  // write it into dst. both buffers are 8192 bytes max.
+  // write it into dst. both buffers are 4096 bytes max.
   // move to beginning of message
   char *end_of_uri;
   char *plus;
   src += strlen("POST /speak?");
-  strncpy(dst, "data: ", MAXLINE);
+  strncpy(dst, "data: ", 4096);
   end_of_uri = strchr(src, ' ');
   strncpy(&dst[6], src, end_of_uri - src);
   strncat(dst, "\n\n", 3);
@@ -91,16 +91,19 @@ typedef enum request_states {
 
 request_type parse_request(recv_buffer *ps) {
   // TODO: based on the bytes in the buffer, return a specific request type.
-  // incomplete should be used when you can't tell what type of request this is, so you have to wait for more bytes.
+  // incomplete should be used when you can't tell what type of request this is,
+  // so you have to wait for more bytes.
   return incomplete;
-  // root should be used when you've received a complete request, and you know it's for index.html.
+  // root should be used when you've received a complete request, and you know
+  // it's for index.html.
   return root;
-  // speak should be used when you've received a complete request, and you know it's a POST message for the resource /speak.
+  // speak should be used when you've received a complete request, and you know
+  // it's a POST message for the resource /speak.
   return speak;
-  // sse_listen should be used when you've received a complete request, and you know it's for the /listen endpoint.
+  // sse_listen should be used when you've received a complete request, and you
+  // know it's for the /listen endpoint.
   return sse_listen;
 }
-
 
 int main(int argc, char **argv) {
   int listenfd, connfd, port, filefd;
@@ -112,11 +115,12 @@ int main(int argc, char **argv) {
     fprintf(stderr, "usage: %s <port>\n", argv[0]);
     exit(0);
   }
-  port = atoi(argv[1]);
   // read index into memory once to send to everyone
   filefd = open("index.html", O_RDONLY);
   read(filefd, indexhtml, 8192);
   close(filefd);
+
+  port = atoi(argv[1]);
   // Open_listenfd calls socket(), bind(), and listen()
   listenfd = Open_listenfd(port);
   init_pool(listenfd, &pool);
@@ -187,17 +191,36 @@ void check_clients(pool *p) {
 
     if ((connfd > 0) && (FD_ISSET(connfd, &p->ready_set))) {
       p->nready--;
-      if ((n = read(connfd, &ps->requestbuf[offset], 8192 - offset)) != 0) {
-        p->protos[i].first_empty_byte += n;
-        fprintf(stderr, "Server received %d bytes on fd %d\n", n, connfd);
-        // TODO: based on the bytes received, move the protocol forward
-        // hint: first decide what to do using parse_request,
-        // then actually do it based on its return value.
-      }
-      /* EOF detected, remove descriptor from pool */
-      else {
-        close_and_remove(p, i);
+      // TODO: for the file descriptor that is ready, accumulate the received
+      // bytes into the right receive buffer at the right offset, and keep
+      // track of how many bytes have been received (so that you can keep
+      // accumulating if more bytes come in).
+      // Hint: don't forget to take care of the special case where the number
+      // of bytes received is zero: that means that the client ended the
+      // conversation (sent EOF).
+
+      // TODO: based on the bytes received, move the protocol forward and
+      // potentially send some bytes back to the client(s).
+      switch (parse_request(ps)) {
+        case incomplete:
+          break;
+        case root:
+          // The server needs to respond to requests for the index by sending a
+          // valid HTTP response along with the content of the index file.
+          break;
+        case speak:
+          // The server needs to respond with an OK message, and send this message
+          // to every currently listening client.
+          break;
+        case sse_listen:
+          // Don't forget to add this socket to the "listening sockets that
+          // should receive chat messages" list, and send back the relevant OK
+          // message for a stream request.
+          break;
+        default:
+          // If we recieve a full request but don't know what to do with it,
+          // send back NOTOK and end the conversation.
+          break;
       }
     }
   }
-}
